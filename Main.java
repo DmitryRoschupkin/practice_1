890b50e0cfc5dc24bfcb83a24ef7787def937a04
DmitryRoschupkin <dmitrii.roshupkin@gmail.com>
2025-12-09 13:09:58 +0500

    created new classes for my project, added some attributes and methods to Pet class

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
 package practice_1;
 
+
+class Box{
+	int a, b, c;
+	int volume(){
+		return a*b*c;
+	}
+}
+
 class Main{
 	public static void main(String[] args){
-		System.out.println("");
+		Box box = new Box();
+		box.a = 10;
+		box.b = 10;
+		box.c = 10;
+		System.out.println(box.volume());
 	}
 }
---
+++ Pet.java
@@ -0,0 +1,16 @@
+package practice_1;
+import String;
+
+public class Pet{
+	int petId;
+	String name;
+	String species;
+	int age;
+	String ownerName;
+	boolean isYoung(){
+
+	}
+	String getLifeStage(){
+
+	}
+}
